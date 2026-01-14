47de759eb6a0074be1ffaafd2f75f105efbad335
Dilip Chauhan <chauhandilip305@gmail.com>
2026-01-15 00:17:37 +0530

    udpated

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -1,9 +1,13 @@
 return {
    'nvim-orgmode/orgmode',
+   dependencies = { "chipsenkbeil/org-roam.nvim" },
    config = function()
       require('orgmode').setup({
          org_agenda_files = '~/orgfiles/**/*',
          org_default_notes_file = '~/orgfiles/refile.org',
+      })
+      require("org-roam").setup({
+         directory = "~/orgfiles",
       })
    end,
 }
