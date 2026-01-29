34bbe2ceb21a2ac0d9b22ce926b0605a43c91910
Dilip Chauhan <chauhandilip305@gmail.com>
2026-01-30 00:27:56 +0530

    updated neowiki

--- lua/plugins/neowiki.lua
+++ lua/plugins/neowiki.lua
@@ -2,6 +2,7 @@ return {
    "echaya/neowiki.nvim",
    ft = "markdown",
    keys = {
+      { "<leader>wo", "<cmd>lua require('neowiki').open_wiki('notes')<cr>", desc = "Open Wiki" },
       { "<leader>ww", "<cmd>lua require('neowiki').open_wiki()<cr>", desc = "Open Wiki" },
       { "<leader>wW", "<cmd>lua require('neowiki').open_wiki_floating()<cr>", desc = "Open Wiki in Floating Window" },
       { "<leader>wT", "<cmd>lua require('neowiki').open_wiki_new_tab()<cr>", desc = "Open Wiki in Tab" },
