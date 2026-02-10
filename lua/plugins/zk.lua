c8a8ea741644e6984eb863e330f1bd9592ccbb54
Dilip Chauhan <chauhandilip305@gmail.com>
2026-02-10 13:06:23 +0530

    added more keys for zk.nvim

--- lua/plugins/zk.lua
+++ lua/plugins/zk.lua
@@ -29,6 +29,16 @@ return {
 		-- Open the link under the caret.
 		vim.api.nvim_set_keymap("n", "<CR>", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
 
+		-- Open notes.
+		vim.api.nvim_set_keymap("n", "<leader>zo", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", opts)
+		-- Open notes associated with the selected tags.
+		vim.api.nvim_set_keymap("n", "<leader>zt", "<Cmd>ZkTags<CR>", opts)
+
+		-- Search for the notes matching a given query.
+		vim.api.nvim_set_keymap("n", "<leader>zf", "<Cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } }<CR>", opts)
+		-- Search for the notes matching the current visual selection.
+		vim.api.nvim_set_keymap("v", "<leader>zf", ":'<,'>ZkMatch<CR>", opts)
+
 		-- Create a new note after asking for its title.
 		-- This overrides the global `<leader>zn` mapping to create the note in the same directory as the current buffer.
 		vim.api.nvim_set_keymap("n", "<leader>zn", "<Cmd>ZkNew { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>", opts)
