0da72f7157ca957beb1d7d2753c24cec50c0ec56
Dilip Chauhan <chauhandilip305@gmail.com>
2026-02-14 12:59:27 +0530

    update: disabled nord theme

--- lua/plugins/nord.lua
+++ lua/plugins/nord.lua
@@ -1,15 +1,16 @@
 return {
-   'shaunsingh/nord.nvim',
-   config = function ()
-      -- -- Example config in lua
-      -- vim.g.nord_contrast = false
-      -- vim.g.nord_borders = false
-      -- vim.g.nord_disable_background = false
-      -- vim.g.nord_italic = true
-      -- vim.g.nord_uniform_diff_background = true
-      -- vim.g.nord_bold = true
-      --
-      -- -- Load the colorscheme
-      -- require('nord').set()
-   end
+	'shaunsingh/nord.nvim',
+	enabled = false,
+	config = function ()
+		-- -- Example config in lua
+		-- vim.g.nord_contrast = false
+		-- vim.g.nord_borders = false
+		-- vim.g.nord_disable_background = false
+		-- vim.g.nord_italic = true
+		-- vim.g.nord_uniform_diff_background = true
+		-- vim.g.nord_bold = true
+		--
+		-- -- Load the colorscheme
+		-- require('nord').set()
+	end
 }
