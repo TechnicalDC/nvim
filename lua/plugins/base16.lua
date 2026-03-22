72dfccc55a3a81c10dd7f281e33ca4ee9192df60
Dilip Chauhan <chauhandilip305@gmail.com>
2026-03-22 09:41:24 +0530

    update: added highlights

--- lua/plugins/base16.lua
+++ lua/plugins/base16.lua
@@ -12,5 +12,23 @@ return {
 			dapui = true,
 		})
 		require('matugen').setup()
+
+		local hl = vim.api.nvim_set_hl
+		local colors = require('base16-colorscheme').colors
+
+		hl(0, "FloatBorder",           { fg   = colors.base03, bg   = "none"        })
+		hl(0, "FloatTitle",            { fg   = colors.base00, bg   = colors.base08 })
+		hl(0, "WinSeparator", { fg = colors.base02 })
+		hl(0, "DiagnosticUnderlineError", { undercurl = true })
+		hl(0, "DiagnosticUnderlineHint",  { undercurl = true })
+		hl(0, "DiagnosticUnderlineInfo",  { undercurl = true })
+		hl(0, "DiagnosticUnderlineOk",    { undercurl = true })
+		hl(0, "DiagnosticUnderlineWarn",  { undercurl = true })
+		hl(0, "TelescopeBorder",       { link = "FloatBorder"  })
+		hl(0, "TelescopePromptBorder", { link = "FloatBorder"  })
+		hl(0, "TelescopeTitle",        { link = "FloatTitle"   })
+		hl(0, "@org.keyword.todo",     { fg   = colors.base08, bold = true          })
+		hl(0, "@org.keyword.done",     { fg   = colors.base0B, bold = true          })
+		hl(0, "@markup.link.label.markdown_inline", { fg = colors.base0E, underline = true })
 	end,
 }
