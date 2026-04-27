59e93b6584f0ea7563823cc7886a440c97cf4fbc
Dilip Chauhan <chauhandilip305@gmail.com>
2026-04-27 21:26:50 +0530

    updated statuscolumn

--- plugin/10_options.lua
+++ plugin/10_options.lua
@@ -35,6 +35,7 @@ set.foldexpr       = "v:lua.vim.treesitter.foldexpr()"
 set.foldtext       = "v:lua.custom_fold_text()"
 set.helpheight   = 15
 -- set.statuscolumn = "%s%=%{v:virtnum == 0 ? (v:relnum ? v:relnum : v:lnum) : ''} "
+set.statuscolumn = "%s%=%l%C "
 
 -- Special UI symbols. More is set via 'mini.basics' later.
 set.fillchars:append({
