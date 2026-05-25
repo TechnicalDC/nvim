faf9e44d1eecdfdb8b9988c0e42bfeae8fb8b436
Dilip Chauhan <chauhandilip305@gmail.com>
2026-05-26 01:36:31 +0530

    updated

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -18,11 +18,11 @@ return {
 			store_selection_keys = '<C-s>',
 			history = true, -- keep around last snippet local to jump back
 			updateevents = "TextChanged,TextChangedI", -- update changes as you type
-			enable_autosnippets = true,
+			enable_autosnippets = false,
 			ext_opts = {
 				[types.choiceNode] = {
 					active = {
-						virt_text = { { "    󰍡 Choice Node", "LuaSnipNode" } },
+						virt_text = { { " " , "LuaSnipNode" } },
 					},
 				},
 			},
