c83fd6b6db7f2dcced0bb26ee4be6ee1de911bef
Dilip Chauhan <chauhandilip305@gmail.com>
2026-05-30 12:12:51 +0530

    updated starter screen

--- lua/dc/config/starter.lua
+++ lua/dc/config/starter.lua
@@ -28,39 +28,39 @@ starter.setup({
 	content_hooks = {
 		-- starter.gen_hook.adding_bullet(),
 		starter.gen_hook.indexing('all'),
-		function(content)
-			-- Coords
-			local header_width = content_type_width(content, "header")
-			local section_width = content_type_width(content, "section")
-			local item_width = content_type_width(content, "item")
-			local footer_width = content_type_width(content, "footer")
-			local max_width = math.max(header_width, section_width, item_width, footer_width)
-
-			for _, line in ipairs(content) do
-				if not (#line == 0 or (#line == 1 and line[1].string == "")) then
-					local line_str = ""
-					local line_types = {}
-					for _, unit in ipairs(line) do
-						line_str = line_str .. unit.string
-						table.insert(line_types, unit.type)
-					end
-					local line_width = 0
-					for _, type in ipairs(line_types) do
-						if type == "item" or type == "section" then
-							line_width = math.max(item_width, section_width)
-						elseif type == "header" then
-							line_width = header_width
-						elseif type == "footer" then
-							line_width = footer_width
-						end
-					end
-					local left_pad = string.rep(" ", (max_width - line_width) * 0.5)
-
-					table.insert(line, 1, { string = left_pad, type = "empty" })
-				end
-			end
-			return content
-		end,
+		-- function(content)
+		-- 	-- Coords
+		-- 	local header_width = content_type_width(content, "header")
+		-- 	local section_width = content_type_width(content, "section")
+		-- 	local item_width = content_type_width(content, "item")
+		-- 	local footer_width = content_type_width(content, "footer")
+		-- 	local max_width = math.max(header_width, section_width, item_width, footer_width)
+		--
+		-- 	for _, line in ipairs(content) do
+		-- 		if not (#line == 0 or (#line == 1 and line[1].string == "")) then
+		-- 			local line_str = ""
+		-- 			local line_types = {}
+		-- 			for _, unit in ipairs(line) do
+		-- 				line_str = line_str .. unit.string
+		-- 				table.insert(line_types, unit.type)
+		-- 			end
+		-- 			local line_width = 0
+		-- 			for _, type in ipairs(line_types) do
+		-- 				if type == "item" or type == "section" then
+		-- 					line_width = math.max(item_width, section_width)
+		-- 				elseif type == "header" then
+		-- 					line_width = header_width
+		-- 				elseif type == "footer" then
+		-- 					line_width = footer_width
+		-- 				end
+		-- 			end
+		-- 			local left_pad = string.rep(" ", (max_width - line_width) * 0.5)
+		--
+		-- 			table.insert(line, 1, { string = left_pad, type = "empty" })
+		-- 		end
+		-- 	end
+		-- 	return content
+		-- end,
 		starter.gen_hook.aligning('center', 'center'),
 	},
 	query_updaters = [[abcdefghilmoqrstuvwxyz0123456789_-,.ABCDEFGHIJKLMOQRSTUVWXYZ]],
