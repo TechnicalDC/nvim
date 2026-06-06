b8c0cff8484e6d2d2deb4c34229ac4278243bd42
Dilip Chauhan <chauhandilip305@gmail.com>
2026-06-06 21:54:18 +0530

    updated colors

--- lua/matugen.lua
+++ lua/matugen.lua
@@ -3,23 +3,23 @@ local M = {}
 function M.setup()
 	require('base16-colorscheme').setup {
 		-- Background tones
-		base00 = '#191825', -- Default Background
-		base01 = '#2a283e', -- Lighter Background (status bars)
-		base02 = '#262437', -- Selection Background
-		base03 = '#646378', -- Comments, Invisibles
+		base00 = '#1e112c', -- Default Background
+		base01 = '#321d49', -- Lighter Background (status bars)
+		base02 = '#2d1a42', -- Selection Background
+		base03 = '#696074', -- Comments, Invisibles
 		-- Foreground tones
-		base04 = '#afafb6', -- Dark Foreground (status bars)
+		base04 = '#b2afb6', -- Dark Foreground (status bars)
 		base05 = '#f2f2f3', -- Default Foreground
 		base06 = '#f2f2f3', -- Light Foreground
 		base07 = '#f2f2f3', -- Lightest Foreground
 		-- Accent colors
 		base08 = '#fd4663', -- Variables, XML Tags, Errors
-		base09 = '#cc66c6', -- Integers, Constants
-		base0A = '#a15cd6', -- Classes, Search Background
-		base0B = '#6f67e4', -- Strings, Diff Inserted
-		base0C = '#e996e4', -- Regex, Escape Chars
-		base0D = '#9893ec', -- Functions, Methods
-		base0E = '#c596e9', -- Keywords, Storage
+		base09 = '#cc669c', -- Integers, Constants
+		base0A = '#d25cd6', -- Classes, Search Background
+		base0B = '#a167e4', -- Strings, Diff Inserted
+		base0C = '#e996c2', -- Regex, Escape Chars
+		base0D = '#bc93ec', -- Functions, Methods
+		base0E = '#e696e9', -- Keywords, Storage
 		base0F = '#fd4663', -- Deprecated, Embedded Tags
 	}
 end
